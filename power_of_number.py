fc8d97394f27bb00dce496e42d951b7177b5c34e
Tesmod <agboolafarouq11@gmail.com>
2023-08-12 00:50:41 +0100

    Added the python file for the Power of a Number computation

---
+++ power_of_number.py
@@ -0,0 +1,13 @@
+#Python codes to perform the power of a number
+
+base = 2
+exponent = -1
+result = pow(base, exponent)
+print(f"The result of {base} raised to the power of {exponent} is: {result}")
+
+#Another method to perform this is given below which also gives the same result
+
+base = 2
+exponent = 0
+result = base ** exponent
+print(f"The result of {base} raised to the power of {exponent} is: {result}")
